99f47cb978abc28896bda442a92de51a10eed153
Kerry <mrkerrywashington@icloud.com>
2018-08-30 17:03:13 -0700

    Initial Commit

---
+++ Interview/API.swift
@@ -0,0 +1,39 @@
+//
+//  API.swift
+//  Interview
+//
+//  Created by Kerry Washington on 8/29/18.
+//  Copyright © 2018 Zume Pizza, Inc. All rights reserved.
+//
+
+import UIKit
+
+
+class API {
+    
+    let fetchUrl = "https://api.myjson.com/bins/d7wgd"
+    
+    public func fetchMenuWithCompletion() -> Array(Any) {
+    
+//    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"https://api.myjson.com/bins/d7wgd"]];
+//
+//    NSURLSession *session = [NSURLSession sharedSession];
+//    [[session dataTaskWithRequest:request
+//    completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+//
+//    dispatch_async(dispatch_get_main_queue(), ^{
+//    NSArray *resArr = [NSJSONSerialization JSONObjectWithData:data
+//    options:0
+//    error:nil];
+//    block(resArr);
+//    });
+//    }] resume];
+//    }
+//
+//    }
+    
+     let request =
+    }
+    
+
+}
